d5fd18881c6dc852e4ff2a4ac47b45ec2e2a9c19
dev-mane <dmhlabane2@gmail.com>
2023-08-17 20:13:22 +0100

    continuing to deal with the Post Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-import fs from "fs"
-import Link from "next/link"
-
-const getPostsMetadata = () => {
-  const folder = "posts/"
-  const files = fs.readdirSync(folder)
-  const markdownPosts = files.filter((file) => file.endsWith(".md"))
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""))
-  return slugs
-}
+import getPostMetadata from "@/components/getPostMetadata"
+import PostPreview from "@/components/PostPreview"
 
 const Homepage = () => {
-  const postMetadata = getPostsMetadata()
-  const postPreviews = postMetadata.map((slug)=> (
-    <div>
-      <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
-      </Link>
-    </div>
+  const postMetadata = getPostMetadata()
+  const postPreviews = postMetadata.map((post)=> (
+    <PostPreview key={post.slug} {...post} />
   ))
   return <div>{postPreviews}</div>
 }
